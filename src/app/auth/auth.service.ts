845faf2f7b594707ca5ff1b6414848bada424961
Rafał Kruczkowski <rk.rafalkruczkowski@gmail.com>
2023-01-27 16:29:35 +0100

    Error handling method implemented

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@angular/core";
-import {HttpClient} from '@angular/common/http'
+import {HttpClient, HttpErrorResponse} from '@angular/common/http'
 import { catchError } from "rxjs";
 import { throwError } from "rxjs";
 
 export interface AuthResponseData {
+    name: string;
+    role: string;
     token: string;
 }
 
@@ -15,23 +17,28 @@ export class AuthService{
     signup(login: string, password: string){
 
        return this.http.post<AuthResponseData>('https://localhost:5001/users/register',{Username: login, Password: password})
-       .pipe(catchError(errorRes =>
-        { 
-            let errorMessage ='An unknown error ocurred!'; 
-            if(!errorRes.error && !errorRes.error.error){
-                return throwError(errorMessage)
-            }
-            else{
-            errorMessage =errorRes.error.message
-            return throwError(errorMessage);
-                }
-        }))
+       .pipe(catchError(this.handleError))
     }
 
     login(login: string, password:string){
 
         return this.http.post<AuthResponseData>('https://localhost:5001/login',{Username: login, Password: password})
-        .pipe()
+        .pipe(catchError(this.handleError))
+    }
+
+
+    //Private method for handlingErrors
+    private handleError(errorRes:HttpErrorResponse){
+
+        let errorMessage ='An unknown error ocurred!'; 
+        if(!errorRes.error && !errorRes.error.error){
+            return throwError(errorMessage)
+        }
+        else{
+        errorMessage =errorRes.error.message
+        return throwError(errorMessage);
+            }
+
     }
 
     
